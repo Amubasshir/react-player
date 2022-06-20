77eee1406fa0bb68343ea46e828f60d5b311f560
Mubasshir Ahmed <lastmubasshir@gmail.com>
2022-06-20 17:01:45 +0600

    play,pause current song with time added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
 import './styles/app.scss';
+import data from './util';
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, useCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div>
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
